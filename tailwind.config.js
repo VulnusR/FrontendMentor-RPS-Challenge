da09963f5219e2fc24863b2fe74274c3231a6839
Caleb Lockwood <caleb.lockwood626@gmail.com>
2023-06-22 17:14:02 -0600

    header changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,23 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+     fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    screens: {
+      'mobile': '375px',
+      // => @media (min-width: 375px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1366px',
+      // => @media (min-width: 1366px) { ... }
+    }
   },
   plugins: [],
 }
